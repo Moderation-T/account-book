7c206f78df5cee196a3524dc13969de18dd4afd2
yizhiyu <yizhiyu_web@163.com>
2020-03-10 13:40:11 +0800

    :sparkles: 使用 restify + faker 创建mock模块

--- gui/src/utils/fetch.js
+++ gui/src/utils/fetch.js
@@ -1,60 +1,59 @@
 const request = (url, config) => {
-    return fetch(url, config)
-      .then(res => {
-        return res.json();
-      })
-      .catch(res => {
-        // 公共错误处理
-        return res.json();
-      });
-  };
-  
-  // GET请求
-  export const get = url => {
-    return request(url, {method: 'GET'});
-  };
-  
-  // POST请求
-  export const post = (url, data) => {
-    return request(url, {
-      body: JSON.stringify(data),
-      headers: {
-        'content-type': 'application/json',
-      },
-      method: 'POST',
+  return fetch(url, config)
+    .then(res => {
+      return res.json();
+    })
+    .catch(res => {
+      // 公共错误处理
+      return res.json();
     });
-  };
+};
 
-    // PUT请求
-    export const delet = (url, data) => {
-        return request(url, {
-          body: JSON.stringify(data),
-          headers: {
-            'content-type': 'application/json',
-          },
-          method: 'DELETE',
-        });
-      };
+// GET请求
+export const get = url => {
+  return request(url, { method: "GET" });
+};
 
-       // PUT请求
-    export const put = (url, data) => {
-        return request(url, {
-          body: JSON.stringify(data),
-          headers: {
-            'content-type': 'application/json',
-          },
-          method: 'PUT',
-        });
-      };
+// POST请求
+export const post = (url, data) => {
+  return request(url, {
+    body: JSON.stringify(data),
+    headers: {
+      "content-type": "application/json"
+    },
+    method: "POST"
+  });
+};
 
-             // PATCH请求
-    export const patch = (url, data) => {
-        return request(url, {
-          body: JSON.stringify(data),
-          headers: {
-            'content-type': 'application/json',
-          },
-          method: 'PATCH',
-        });
-      };
-  
+// PUT请求
+export const delete = (url, data) => {
+  return request(url, {
+    body: JSON.stringify(data),
+    headers: {
+      "content-type": "application/json"
+    },
+    method: "DELETE"
+  });
+};
+
+// PUT请求
+export const put = (url, data) => {
+  return request(url, {
+    body: JSON.stringify(data),
+    headers: {
+      "content-type": "application/json"
+    },
+    method: "PUT"
+  });
+};
+
+// PATCH请求
+export const patch = (url, data) => {
+  return request(url, {
+    body: JSON.stringify(data),
+    headers: {
+      "content-type": "application/json"
+    },
+    method: "PATCH"
+  });
+};
